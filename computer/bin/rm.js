91c4a26fec1d1afde3632443e6e01b58c78a2c0a
505e06b2 <505e06b2@gmail.com>
2018-07-28 15:53:10 +0100

    rm -r should work propely?

--- computer/bin/rm.js
+++ computer/bin/rm.js
@@ -2,13 +2,30 @@
 	if(args[0]) {
 		var resolved = shell.resolve(args[1]);
 		if(args[0] === "-r" && os.exists(resolved) === "directory") { //go through the dir
-			var dir_contents = os.listdir(resolved); //returns an array of files
-			dir_contents.some(function(e) {
-				if(!os.remove(resolved + "/" + e)) {
-					os.print("Couldn't remove: '" + resolved + "/" + e + "'");
-					return true;
-				}
-			});
+		
+			var recurse_dirs = function(dir) {
+				var dir_contents = os.listdir(dir); //returns an array of files
+				dir_contents.some(function(e) {
+					var current = dir + "/" + e;
+					var type = os.exists(current);
+					
+					switch(type) {
+						case "directory":
+							recurse_dirs(current);
+							return false;
+						case "file":
+							if(!os.remove(current)) {
+								os.print("Couldn't remove: '" + args[0] + "'");
+								return true;
+							}
+							return false;
+						default: //kill if undefined
+							return true;
+					}
+				});
+			}
+			
+			recurse_dirs(resolved);
 			os.rmdir(resolved);
 			
 		} else {
