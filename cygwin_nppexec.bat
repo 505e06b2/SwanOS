abc1e32bedc57aa695726407dd953b8cdaeae8da
505e06b2 <505e06b2@gmail.com>
2018-07-30 17:24:53 +0100

    Removed more of my macros

--- cygwin_nppexec.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set PROJECT_PATH=%~dp0
-set COMPILE=C:\cygwin64\bin\bash --login "/cygdrive/c/Users/x/Home/Projects/JS Term/macros/compile.sh"
-set RUN=C:\cygwin64\bin\bash --login "/cygdrive/c/Users/x/Home/Projects/JS Term/macros/run.sh"
-
-cd "C:\Program Files\Notepad++\"
-start notepad++.exe
